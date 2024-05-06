e009a9bbab33dd3bb4783ec9013e8a386c372ac2
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-06 16:04:15 +0000

    prepare providers

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -8,16 +8,19 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-06 18:08:18
+ * @lastupdate 2024-05-06 23:00:30
  */
 
 namespace Diepxuan\Providers;
 
+use Composer\InstalledVersions as ComposerPackage;
 use Diepxuan\Http\Kernel;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Str;
 
-class DiepxuanServiceProvider extends ServiceProvider
+class ServiceProvider extends BaseServiceProvider
 {
     protected string $moduleName      = 'diepxuan/laravel-core';
     protected string $moduleNameLower = 'core';
@@ -27,12 +30,6 @@ class DiepxuanServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerCommands();
-        $this->registerCommandSchedules();
-        $this->registerTranslations();
-        $this->registerConfig();
-        $this->registerMiddlewares();
-        $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->moduleName, 'database/migrations'));
     }
 
@@ -41,39 +38,15 @@ class DiepxuanServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // @todo
-    }
+        dd($this->packages());
 
-    /**
-     * Register translations.
-     */
-    public function registerTranslations(): void
-    {
-        $langPath = resource_path('lang/modules/' . $this->moduleNameLower);
-
-        if (is_dir($langPath)) {
-            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom($langPath);
-        } else {
-            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
-            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
-        }
-    }
-
-    /**
-     * Register views.
-     */
-    public function registerViews(): void
-    {
-        $viewPath   = resource_path('views/modules/' . $this->moduleNameLower);
-        $sourcePath = module_path($this->moduleName, 'resources/views');
-
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->moduleNameLower . '-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
-
-        $componentNamespace = str_replace('/', '\\', $this->moduleName);
-        Blade::componentNamespace($componentNamespace, $this->moduleNameLower);
+        $this->registerConfig()
+            ->registerMiddlewares()
+            ->registerCommands()
+            ->registerCommandSchedules()
+            ->registerViews()
+            ->registerTranslations()
+        ;
     }
 
     /**
@@ -87,40 +60,95 @@ class DiepxuanServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register config.
+     * Register translations.
      */
-    protected function registerConfig(): void
+    protected function registerTranslations()
     {
-        $this->publishes([module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower . '.php')], 'config');
-        $this->mergeConfigFrom(module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower);
+        $langPath = resource_path('lang/modules/' . $this->moduleNameLower);
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, $this->moduleNameLower);
+            $this->loadJsonTranslationsFrom($langPath);
+        } else {
+            $this->loadTranslationsFrom(module_path($this->moduleName, 'lang'), $this->moduleNameLower);
+            $this->loadJsonTranslationsFrom(module_path($this->moduleName, 'lang'));
+        }
+
+        return $this;
     }
 
     /**
-     * Register commands in the format of Command::class.
+     * Register views.
      */
-    protected function registerCommands(): void
+    protected function registerViews()
+    {
+        $viewPath   = resource_path('views/modules/' . $this->moduleNameLower);
+        $sourcePath = module_path($this->moduleName, 'resources/views');
+
+        $this->publishes([$sourcePath => $viewPath], ['views', $this->moduleNameLower . '-module-views']);
+
+        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->moduleNameLower);
+
+        $componentNamespace = str_replace('/', '\\', $this->moduleName);
+        Blade::componentNamespace($componentNamespace, $this->moduleNameLower);
+
+        return $this;
+    }
+
+    protected function packages(): array
+    {
+        return Collection::wrap(ComposerPackage::getInstalledPackages())
+            ->where(static fn (string $value) => Str::of($value)
+                ->startsWith('diepxuan'))
+            ->where(static fn (string $value) => !Str::of($value)
+                ->is(ComposerPackage::getRootPackage()['name']))
+            ->all()
+        ;
+    }
+
+    /**
+     * Register config.
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([module_path($this->moduleName, 'config/config.php') => config_path($this->moduleNameLower . '.php')], 'config');
+        $this->mergeConfigFrom(module_path($this->moduleName, 'config/config.php'), $this->moduleNameLower);
+
+        return $this;
+    }
+
+    /**
+     * Register Commands.
+     */
+    protected function registerCommands()
     {
         // $this->commands([]);
+
+        return $this;
     }
 
     /**
      * Register command Schedules.
      */
-    protected function registerCommandSchedules(): void
+    protected function registerCommandSchedules()
     {
         // $this->app->booted(function () {
         //     $schedule = $this->app->make(Schedule::class);
         //     $schedule->command('inspire')->hourly();
         // });
+
+        return $this;
     }
 
     /**
      * Register Middlewares.
      */
-    protected function registerMiddlewares(): void
+    protected function registerMiddlewares()
     {
         $kernel = new Kernel();
         $kernel->load();
+
+        return $this;
     }
 
     /**
